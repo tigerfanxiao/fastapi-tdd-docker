c46240c43e9636c28a42b2b3d94c0ff6d6850ace
xiao fan <tigerfanxiao@gmail.com>
2024-10-19 23:33:01 +0200

    remove exclamation of pong

--- project/app/tests/test_ping.py
+++ project/app/tests/test_ping.py
@@ -25,4 +25,4 @@ def test_app():
 def test_ping(test_app):
     response = test_app.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
